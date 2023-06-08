73787d5f4f50560b390c3d51679410e5c691862a
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-08 11:07:09 +0200

    men view controller

---
+++ iosAlphaEcommerce/menViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenViewController.swift
+//  iosAlphaEcommerce
+//
+//  Created by DA MAC M1 139 on 2023/06/08.
+//
+
+import UIKit
+
+class menViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
