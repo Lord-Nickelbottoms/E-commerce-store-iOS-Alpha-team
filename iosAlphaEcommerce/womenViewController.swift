30aaa6f1f13d1c13ae22296df6047b739f94fbcb
Gomotsegang1 <nadinematloa@gmail.com>
2023-06-08 11:09:42 +0200

    added the womenViewController

---
+++ iosAlphaEcommerce/womenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  womenViewController.swift
+//  iosAlphaEcommerce
+//
+//  Created by DA MAC M1 163 on 2023/06/08.
+//
+
+import UIKit
+
+class womenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
