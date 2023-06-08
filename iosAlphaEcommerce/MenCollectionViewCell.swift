70cc84c8579c386a4cd29d20d21a048c11ed45f6
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-08 13:37:12 +0200

    Updated collectionView cell

--- iosAlphaEcommerce/MenCollectionViewCell.swift
+++ iosAlphaEcommerce/MenCollectionViewCell.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class MenCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
     
+    @IBOutlet weak var lblName: UILabel!
+    
+    func setup(with item: Item)
+    {
+        productImageView.image = item.image
+        lblName.text = item.name
+    }
 }
