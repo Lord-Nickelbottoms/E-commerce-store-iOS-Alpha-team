1cc99e7ed951feeafb5480433b4dc4b922ea8eb1
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-08 12:33:10 +0200

    Clothing added

---
+++ iosAlphaEcommerce/MenCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenCollectionViewCell.swift
+//  iosAlphaEcommerce
+//
+//  Created by DA MAC M1 139 on 2023/06/08.
+//
+
+import UIKit
+
+class MenCollectionViewCell: UICollectionViewCell {
+    
+}
