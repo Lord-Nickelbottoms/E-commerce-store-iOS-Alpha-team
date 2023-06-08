d746a4377af7791b6bc960005e08ec7f6305600e
Gomotsegang1 <nadinematloa@gmail.com>
2023-06-08 09:02:57 +0200

    first commit

---
+++ iosAlphaEcommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosAlphaEcommerce
+//
+//  Created by DA MAC M1 163 on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
