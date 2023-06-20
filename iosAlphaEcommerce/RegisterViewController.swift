05c5641798e10cfd7c56dfdfb94d300005f3a9b0
Lord-Nickelbottoms <nizaam.h@icloud.com>
2023-06-20 12:34:56 +0200

    Connected textfields and buttons in Login and Register pages

--- iosAlphaEcommerce/RegisterViewController.swift
+++ iosAlphaEcommerce/RegisterViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+	
+	
+	@IBOutlet weak var firstNameTextField: UITextField!
+	@IBOutlet weak var lastNameTextField: UITextField!
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var contactTextField: UITextField!
+	@IBOutlet weak var addressTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var confirmPasswordTextField: UITextField!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	
+	@IBAction func registerButton(_ sender: UIButton) {
+	}
+	
 
 }
