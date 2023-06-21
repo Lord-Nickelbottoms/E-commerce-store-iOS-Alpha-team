9d7f978b4d739c1ec3682ae0294ec4ecdd610ca7
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-21 15:34:10 +0200

    login check password and username

--- iosAlphaEcommerce/LoginViewController.swift
+++ iosAlphaEcommerce/LoginViewController.swift
@@ -14,12 +14,40 @@ class LoginViewController: UIViewController {
 	
 	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
+    var user: Users!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 		activityIndicator.isHidden.toggle()
+        fetchApiData {[self] in
+            if(emailTextField.text == user.email && passwordTextField.text == user.password)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
     }
-    
+    func fetchApiData(completed: @escaping() -> ()){
+        let url = URL(string: "https://fakestoreapi.com/users/1")
+        URLSession.shared.dataTask(with: url!) {data, result, error in
+            
+            if error == nil{
+                do{
+                    self.user = try JSONDecoder().decode(Users.self, from: data!)
+                }
+                catch{
+                    print("Error fetching data from API.")
+                }
+                
+                DispatchQueue.main.async {
+                    completed()
+                }
+            }
+        }.resume()
+    }
 
 	@IBAction func loginButton(_ sender: UIButton) {
 		
@@ -33,7 +61,26 @@ class LoginViewController: UIViewController {
 			activityIndicator.stopAnimating()
 			activityIndicator.isHidden.toggle()
 		}
+        fetchApiData {[self] in
+            if(emailTextField.text == user.email && passwordTextField.text == user.password)
+            {
+                
+            }
+            else
+            {
+                addAlert(message: "Invalid password or email", title: "Invalid password")
+            }
+            
+        }
 	}
-    
+    func addAlert(message: String, title: String) {
+        let alert = UIAlertController(title: title, message: message,preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: { _ in
+            //Cancel Action
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
+
